ebeb6630e90a602c9bf3ade667f9f83a21fc1deb
Sachmansahni <raunaksahni71@gmail.com>
2024-11-25 09:25:15 +0530

    access refresh generators added

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -42,4 +42,31 @@ userSchema.methods.isPasswordCorrect=async function(password){
     return await bcrypt.compare(password,this.password)
 }
 
+userSchema.methods.generateAccessToken=function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            name:this.name,
+            phone:this.phone,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIREY
+        }
+    )
+}
+
+
+userSchema.methods.generateRefreshToken=function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIREY
+        }
+    )
+}
 export const User=mongoose.model("User",userSchema)

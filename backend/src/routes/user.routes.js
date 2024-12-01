18b160c4265bcc049a11ceb19285c4466279dbfe
Sachmansahni <raunaksahni71@gmail.com>
2024-12-01 10:12:38 +0530

    smart search route added

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { smartSearch } from "../controllers/smartsearch.controller.js";
 
 const router=Router()
 
 router.route("/signup").post(registerUser)
-
-
+router.route("/login").post(loginUser)
+router.route("/smartsearch").post(smartSearch)
 
 export default router
